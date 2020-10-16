58697a0aee6d86b3e4a73a7348a0d7737c4b03c5
JailsonS <engsoaresfilho@gmail.com>
2020-10-16 13:48:01 -0300

    creating models

---
+++ app/Models/BarbersTestimonial.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarbersTestimonial extends Model
+{
+    use HasFactory;
+    protected $table = 'barberstestimonials';
+    public $timestamps = false;
+}
